1675bf8ee914bdce60388803cef094c006a3cf57
Bernhard Kau <bernhard@kau-boys.de>
2021-10-21 00:23:05 +0200

    adding some escaping functions to the functions.php and footer.php files

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
 </div><!-- end wrap -->
 
 	<footer id="colophon" class="clearfix">
-		<p>Proudly powered by <a href="http://wordpress.org/">WordPress</a><span class="sep"> | </span><?php printf( __( 'Theme: %1$s by %2$s', 'yoko' ), 'Yoko', '<a href="http://www.elmastudio.de/en/themes/">Elmastudio</a>' ); ?></p>
+		<p>Proudly powered by <a href="http://wordpress.org/">WordPress</a><span class="sep"> | </span><?php printf( esc_html__( 'Theme: %1$s by %2$s', 'yoko' ), 'Yoko', '<a href="http://www.elmastudio.de/en/themes/">Elmastudio</a>' ); ?></p>
 		<?php
         // Footer navigation menu.
         wp_nav_menu( array(
@@ -16,7 +16,7 @@
             'theme_location' => 'footer',
         ) );
         ?>
-		<a href="#page" class="top"><?php _e( 'Top', 'yoko' ); ?></a>
+		<a href="#page" class="top"><?php esc_html_e( 'Top', 'yoko' ); ?></a>
 	</footer><!-- end colophon -->
 	
 </div><!-- end page -->
